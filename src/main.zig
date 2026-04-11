e36a44833cfc05084694d7ccd1ae9a355fc633fb
munastronaut <96724656+munastronaut@users.noreply.github.com>
2026-04-11 22:36:01 +0800

    Remove all usages of `try` in `main`

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 const Io = std.Io;
 const Allocator = std.mem.Allocator;
 const path = std.fs.path;
+const fatal = std.process.fatal;
 
 const Lexer = @import("Lexer.zig");
 
@@ -10,6 +11,8 @@ var artifact_buf: [4096]u8 align(std.heap.page_size_min) = undefined;
 
 fn errorDescription(err: anyerror) []const u8 {
     return switch (err) {
+        error.WriteFailed => "failed to write to file",
+        error.OutOfMemory => "ran out of memory",
         error.FileNotFound => "no such file or directory",
         error.NotDir => "not a directory",
         error.IsDir => "is a directory",
@@ -17,32 +20,35 @@ fn errorDescription(err: anyerror) []const u8 {
     };
 }
 
-fn fatalError(name: []const u8, err: anyerror) noreturn {
-    const desc = errorDescription(err);
-    std.process.fatal("{s}: {s}", .{ name, desc });
+fn fatalErrorFilename(name: []const u8, err: anyerror) noreturn {
+    fatal("{s}: {s}", .{ name, errorDescription(err) });
+}
+
+fn fatalError(err: anyerror) noreturn {
+    fatal("{s}", .{errorDescription(err)});
 }
 
 fn openDirIfAbs(io: Io, name: []const u8) ?Io.Dir {
     if (path.isAbsolute(name)) {
         const dirname = path.dirname(name).?;
-        return Io.Dir.openDirAbsolute(io, dirname, .{}) catch |err| fatalError(dirname, err);
+        return Io.Dir.openDirAbsolute(io, dirname, .{}) catch |err| fatalErrorFilename(dirname, err);
     }
     return null;
 }
 
-pub fn main(init: std.process.Init) !void {
+pub fn main(init: std.process.Init) void {
     const env = init.environ_map;
     const io = init.io;
     const allocator = init.arena.allocator();
 
     var args = init.minimal.args.iterate();
     _ = args.next();
-    const input = args.next() orelse std.process.fatal("no input file", .{});
-    const output = args.next() orelse try std.mem.concat(allocator, u8, &.{ path.stem(input), ".js" });
+    const input = args.next() orelse fatal("no input file", .{});
+    const output = args.next() orelse std.mem.concat(allocator, u8, &.{ path.stem(input), ".js" }) catch |err| fatalError(err);
 
     const src_dir: Io.Dir = openDirIfAbs(io, input) orelse .cwd();
 
-    const src = src_dir.readFileAllocOptions(io, input, allocator, .limited(std.math.maxInt(u32)), .of(u8), 0) catch |err| fatalError(input, err);
+    const src = src_dir.readFileAllocOptions(io, input, allocator, .limited(std.math.maxInt(u32)), .of(u8), 0) catch |err| fatalErrorFilename(input, err);
 
     var stdout_w = Io.File.stdout().writer(io, &stdout_buf);
 
@@ -53,12 +59,12 @@ pub fn main(init: std.process.Init) !void {
 
     const no_color = env.contains("NO_COLOR");
 
-    const term_mode = try Io.Terminal.Mode.detect(
+    const term_mode = Io.Terminal.Mode.detect(
         io,
         stdout_w.file,
         no_color,
         clicolor_force,
-    );
+    ) catch .no_color;
 
     const t: Io.Terminal = .{
         .mode = term_mode,
@@ -69,17 +75,17 @@ pub fn main(init: std.process.Init) !void {
     var tok_count: usize = 1;
     while (true) : (tok_count += 1) {
         const tok = lexer.next();
-        try lexer.output(t, tok, tok_count);
+        lexer.output(t, tok, tok_count) catch |err| fatalError(err);
         if (tok.id == .eof) break;
     }
-    try t.writer.flush();
+    t.writer.flush() catch |err| fatalError(err);
 
     const artifact_dir: Io.Dir = openDirIfAbs(io, output) orelse .cwd();
 
-    var artifact_file = try artifact_dir.createFile(io, output, .{});
+    var artifact_file = artifact_dir.createFile(io, output, .{}) catch |err| fatalError(err);
     var artifact_w = artifact_file.writer(io, &artifact_buf);
     const artifact = &artifact_w.interface;
 
-    try artifact.writeAll("const noop = () => {};\nnoop();");
-    try artifact.flush();
+    artifact.writeAll("const noop = () => {};\nnoop();") catch |err| fatalError(err);
+    artifact.flush() catch |err| fatalError(err);
 }
