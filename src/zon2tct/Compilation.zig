c72fe2ec5ba98a8591418aa2f778290bd33909fd
munastronaut <96724656+munastronaut@users.noreply.github.com>
2026-04-25 10:50:15 +0800

    Add `testing` preset

--- src/zon2tct/Compilation.zig
+++ src/zon2tct/Compilation.zig
@@ -10,3 +10,11 @@ arena: Allocator,
 io: std.Io,
 cwd: std.Io.Dir,
 diagnostics: *Diagnostics,
+
+pub const testing: Compilation = .{
+    .gpa = std.testing.allocator,
+    .arena = undefined,
+    .io = std.testing.io,
+    .cwd = undefined,
+    .diagnostics = undefined,
+};
