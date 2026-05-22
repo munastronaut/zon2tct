ca82eaa978507c87c03626e839390d0b9f2ecdc3
munastronaut <96724656+munastronaut@users.noreply.github.com>
2026-05-22 12:15:04 +0800

    Add braces, nothing rlly significant

--- build.zig
+++ build.zig
@@ -27,6 +27,7 @@ pub fn build(b: *std.Build) void {
     run_step.dependOn(&run_cmd.step);
     run_cmd.step.dependOn(b.getInstallStep());
 
-    if (b.args) |args|
+    if (b.args) |args| {
         run_cmd.addArgs(args);
+    }
 }
