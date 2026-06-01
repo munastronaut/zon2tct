9f4259b934504ef9a2ddbafccef617e4d51b434e
munastronaut <96724656+munastronaut@users.noreply.github.com>
2026-06-01 13:42:32 +0800

    Remove `IoMode`

--- build.zig
+++ build.zig
@@ -2,8 +2,6 @@ const std = @import("std");
 
 const zon2tct_version: std.SemanticVersion = .{ .major = 0, .minor = 3, .patch = 0 };
 
-const IoMode = enum { threaded, evented };
-
 pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
