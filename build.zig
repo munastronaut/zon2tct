bf2c2486f75dbf0b3abb06852a161fb9a0922575
munastronaut <96724656+munastronaut@users.noreply.github.com>
2026-07-26 21:02:12 +0800

    Make own archiver that actually outputs zip and tar files

--- build.zig
+++ build.zig
@@ -1,18 +1,17 @@
 const std = @import("std");
+const Io = std.Io;
+const Build = std.Build;
 
 const zon2tct_version: std.SemanticVersion = .{ .major = 0, .minor = 4, .patch = 0 };
 
-pub fn build(b: *std.Build) !void {
+pub fn build(b: *Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
     const exe = addCompilerStep(b, .{
         .optimize = optimize,
         .target = target,
-        .strip = switch (optimize) {
-            .Debug, .ReleaseSafe => false,
-            .ReleaseFast, .ReleaseSmall => true,
-        },
+        .strip = !optimize.runtimeSafety(),
     });
 
     const zon2tct_mod = b.addModule("zon2tct", .{
@@ -44,15 +43,15 @@ pub fn build(b: *std.Build) !void {
 
     const release_step = b.step("release", "Build the application for release targets");
 
-    const dist_path = b.graph.path(.install_prefix, "dist");
-    const make_dist_cmd = dist: {
-        const cmd = b.addSystemCommand(&.{"mkdir"});
-        if (b.graph.host.result.os.tag != .windows) {
-            cmd.addArg("-p");
-        }
-        cmd.addDirectoryArg(dist_path);
-        break :dist cmd;
-    };
+    //const dist_path = b.graph.path(.install_prefix, "dist");
+    //const make_dist_cmd = dist: {
+    //    const cmd = b.addSystemCommand(&.{"mkdir"});
+    //    if (b.graph.host.result.os.tag != .windows) {
+    //        cmd.addArg("-p");
+    //    }
+    //    cmd.addDirectoryArg(dist_path);
+    //    break :dist cmd;
+    //};
 
     const tgts = [_]std.Target.Query{
         .{ .cpu_arch = .x86_64, .os_tag = .linux },
@@ -62,7 +61,16 @@ pub fn build(b: *std.Build) !void {
         .{ .cpu_arch = .aarch64, .os_tag = .macos },
     };
 
-    const release_opt: std.lang.OptimizeMode = .ReleaseSmall;
+    const release_opt: std.lang.OptimizeMode = .small;
+
+    const archiver_exe = b.addExecutable(.{
+        .name = "archiver",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("tools/archiver.zig"),
+            .target = b.graph.host,
+            .optimize = .fast,
+        }),
+    });
 
     for (tgts) |tgt| {
         const res_tgt = b.resolveTargetQuery(tgt);
@@ -70,13 +78,13 @@ pub fn build(b: *std.Build) !void {
         const release_exe = addCompilerStep(b, .{
             .optimize = release_opt,
             .target = res_tgt,
-            .strip = switch (release_opt) {
-                .Debug, .ReleaseSafe => false,
-                .ReleaseFast, .ReleaseSmall => true,
-            },
+            .strip = !release_opt.runtimeSafety(),
         });
         release_exe.root_module.addImport("zon2tct", zon2tct_mod);
         release_exe.root_module.addOptions("build_options", exe_options);
+
+        const is_windows = res_tgt.result.os.tag == .windows;
+        const archive_ext = if (is_windows) ".zip" else ".tar.gz";
 
         const tgt_str = b.fmt("{t}-{t}", .{ res_tgt.result.cpu.arch, res_tgt.result.os.tag });
         const archive_name = b.fmt("zon2tct-{s}-{d}.{d}.{d}", .{
@@ -86,35 +94,24 @@ pub fn build(b: *std.Build) !void {
             zon2tct_version.patch,
         });
 
-        const exe_file = release_exe.getEmittedBin();
+        const archive_filename = b.fmt("{s}{s}", .{ archive_name, archive_ext });
 
-        const compress_cmd = b.addSystemCommand(&.{"tar"});
-        compress_cmd.step.dependOn(&make_dist_cmd.step);
+        const archive_cmd = b.addRunArtifact(archiver_exe);
+        archive_cmd.addFileArg(release_exe.getEmittedBin());
+        const archived_file = archive_cmd.addOutputFileArg2(b.fmt("dist/{s}{s}", .{ archive_name, archive_ext }), .{});
 
-        const archive_extension = if (res_tgt.result.os.tag == .windows) ".zip" else ".tar.gz";
-        const archive_file = b.fmt("{s}{s}", .{ archive_name, archive_extension });
-        const exe_filename = if (res_tgt.result.os.tag == .windows) "zon2tct.exe" else "zon2tct";
-        const out_path = dist_path.path(b, archive_file);
-
-        const flags = if (res_tgt.result.os.tag == .windows) "-acf" else "-czf";
-        compress_cmd.addArg(flags);
-        compress_cmd.addFileArg(out_path);
-        compress_cmd.addPrefixedFileArg("-C", exe_file.dirname());
-        compress_cmd.addArg(exe_filename);
-
-        compress_cmd.step.dependOn(&release_exe.step);
-
-        release_step.dependOn(&compress_cmd.step);
+        const archive = b.addInstallFileWithDir(archived_file, .{ .custom = "dist" }, archive_filename);
+        release_step.dependOn(&archive.step);
     }
 }
 
 const AddCompilerModOptions = struct {
     optimize: std.lang.OptimizeMode,
-    target: std.Build.ResolvedTarget,
+    target: Build.ResolvedTarget,
     strip: ?bool = null,
 };
 
-fn addCompilerMod(b: *std.Build, options: AddCompilerModOptions) *std.Build.Module {
+fn addCompilerMod(b: *Build, options: AddCompilerModOptions) *Build.Module {
     const compiler_mod = b.createModule(.{
         .root_source_file = b.path("src/main.zig"),
         .target = options.target,
@@ -125,7 +122,7 @@ fn addCompilerMod(b: *std.Build, options: AddCompilerModOptions) *std.Build.Modu
     return compiler_mod;
 }
 
-fn addCompilerStep(b: *std.Build, options: AddCompilerModOptions) *std.Build.Step.Compile {
+fn addCompilerStep(b: *Build, options: AddCompilerModOptions) *Build.Step.Compile {
     return b.addExecutable(.{
         .name = "zon2tct",
         .root_module = addCompilerMod(b, options),
