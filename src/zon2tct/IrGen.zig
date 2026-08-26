31218dffa530028ad67015a2e1de189d5d7ae9d4
munastronaut <96724656+munastronaut@users.noreply.github.com>
2026-08-26 17:04:21 +0800

    Don't spam user with errors if they fail to correctly define a def

--- src/zon2tct/IrGen.zig
+++ src/zon2tct/IrGen.zig
@@ -366,13 +366,19 @@ fn lowerDefinitions(ig: *IrGen, def_node: Tree.Node.Index) Allocator.Error!void 
                 }
 
                 if (ig.identAsString(ident_tok)) |name_str| {
-                    const value = try ig.resolvePk(val_node, table) orelse continue;
-                    const gop = try table.getOrPutContext(ig.gpa, @backingInt(name_str), StringIndexContext{ .bytes = &ig.string_bytes });
+                    const value = try ig.resolvePk(val_node, table);
+                    const gop = try table.getOrPutContext(
+                        ig.gpa,
+                        @backingInt(name_str),
+                        StringIndexContext{ .bytes = &ig.string_bytes },
+                    );
                     if (gop.found_existing) {
                         const raw_str = name_str.getAny(ig.string_bytes.items);
                         try ig.addErrorTok(ident_tok, "duplicate definition for '{s}'", .{raw_str});
                     } else {
-                        gop.value_ptr.* = value;
+                        // We use 4294967295 as the poison value here because no modder in their
+                        // right mind would use that as an actual primary key.
+                        gop.value_ptr.* = if (value) |pk| pk else std.math.maxInt(u32);
                     }
                 } else |err| switch (err) {
                     error.BadString => {},
